3e9f552f1390baab31df0cc4b768bfbd345cceed
James Wyn <thisisjameswyn@gmail.com>
2021-06-04 09:50:17 -0700

    add Treat values

--- SweetSavory/Models/Treat.cs
+++ SweetSavory/Models/Treat.cs
@@ -6,7 +6,14 @@ namespace SweetSavory.Models
   {
     public Treat()
     {
-
+      this.JoinEntities = new HashSet<FlavorTreat>();
     }
+
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public virtual ApplicationUser User { get; set; }
+
+    public virtual ICollection<FlavorTreat> JoinEntities { get;}
   }
 }
